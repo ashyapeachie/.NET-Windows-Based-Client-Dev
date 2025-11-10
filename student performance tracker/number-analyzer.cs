526bd770aa3973670ba62e10b3b4a3adeecaa261
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-11-10 14:23:47 -0500

    changing location

---
+++ student performance tracker/number-analyzer.cs
@@ -0,0 +1,21 @@
+// goal: for, while, and foreach loops
+// remember to add proper header 
+
+using System;
+using System.Linq;
+
+namespace NumberAnalyzer {
+    class Program {
+        static void Main() {
+            // Console.WriteLine();
+            // Console.WriteLine();
+            // string input = Console.ReadLine();
+
+
+            // splitting input and convert to int array
+
+
+            // sum and average using for Loop
+        }
+    }
+}
