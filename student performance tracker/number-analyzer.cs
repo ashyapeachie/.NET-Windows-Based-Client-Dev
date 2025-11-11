a103f16ece75bb6f41da23b9883416cb88742e17
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-11-11 00:36:14 -0500

    adding for loops, foreach, printing statements, and commenting for later code

--- student performance tracker/number-analyzer.cs
+++ student performance tracker/number-analyzer.cs
@@ -19,18 +19,33 @@ namespace NumberAnalyzer {
             int[] numbers = new int[parts.Length];
             // splitting input and convert to int array
 
-            for () {
-
+            for (int i = 0; i < parts.Length; i++) {
+                int.TryParse(parts[i].Trim(), out numbers[i]);
             }
 
-            for () {
-
-            }
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            } 
+            // double average 
             // sum and average using for Loop
             
             int largest = numbers[0];
             int smallest = numbers[0];
             // finding largest and smallest 
+
+            foreach (int n in numbers)
+            {
+
+            }
+
+
+            // reversing the output while loop
+
+            Console.WriteLine(); // sum
+            Console.WriteLine(); // avg
+            Console.WriteLine(); // largest
+            Console.WriteLine(); // smallest
         }
     }
 }
