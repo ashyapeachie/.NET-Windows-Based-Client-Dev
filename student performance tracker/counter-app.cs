eb136833be3211d28ef6ca48058f69a93f31955a
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-11-11 00:09:47 -0500

    fixing header

--- student performance tracker/counter-app.cs
+++ student performance tracker/counter-app.cs
@@ -1,7 +1,7 @@
 /* 
-Author:
-Filename:
-Description:
+Author: Aham
+Filename: counter-app.cs, Form1.cs
+Description: Windows Forms app created to practice basic event handline in a GUI
  */
 
 // goal: practice basic event handling in a GUI
