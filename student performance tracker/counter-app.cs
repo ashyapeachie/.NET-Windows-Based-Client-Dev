d94051781396306f451f126c7ce4483ab04d0c3d
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-11-11 00:35:14 -0500

    code setup

--- student performance tracker/counter-app.cs
+++ student performance tracker/counter-app.cs
@@ -4,8 +4,12 @@ Filename: counter-app.cs, Form1.cs
 Description: Windows Forms app created to practice basic event handline in a GUI
  */
 
-// goal: practice basic event handling in a GUI
-// add proper header
-
 using System;
 using System.Windows.Forms;
+
+namespace counterapp {
+    // class {
+        // button 
+        // button 
+    // }
+}
